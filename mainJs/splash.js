e67d6ea63924ccda2ea02e36b3116c276ed217e4
Sam <gudwbot@gmail.com>
2024-11-25 15:46:00 +0330

    t3

--- mainJs/splash.js
+++
@@ -1,5 +0,0 @@
-// Change this path to the directory and HTML file you want to redirect to
-const redirectPath = "./en/index.html";
-
-// Perform the redirect
-setTimeout(() => {window.location.href = redirectPath},3000);
